c7c548e881ea84497ca75e8092ff42fd20ac662b
kwakjaehun <jh5414092@icloud.com>
2025-01-29 03:26:30 +0900

    Chore: 기타 수정

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -9,10 +9,73 @@ import { Editor, EditorContent, JSONContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import "./editor.css";
+
+interface CategoryData {
+  blog: string;
+  categories: string[];
+}
+
+const dummyCategoriesData: CategoryData[] = [
+  {
+    blog: "IT",
+    categories: ["Phone", "Desktop", "Mobile", "Tablet", "Gadget"],
+  },
+  {
+    blog: "Technology",
+    categories: [
+      "Artificial Intelligence",
+      "Robotics",
+      "Quantum Computing",
+      "IoT",
+      "Blockchain",
+    ],
+  },
+  {
+    blog: "Lifestyle",
+    categories: [
+      "Fitness",
+      "Healthy Eating",
+      "Mental Health",
+      "Travel",
+      "Personal Growth",
+    ],
+  },
+  {
+    blog: "Finance",
+    categories: [
+      "Investing",
+      "Personal Finance",
+      "Cryptocurrency",
+      "Stock Market",
+      "Real Estate",
+    ],
+  },
+  {
+    blog: "Entertainment",
+    categories: ["Movies", "TV Shows", "Music", "Gaming", "Books"],
+  },
+  {
+    blog: "Science",
+    categories: [
+      "Physics",
+      "Biology",
+      "Chemistry",
+      "Astronomy",
+      "Environmental Science",
+    ],
+  },
+];
 
 const BlogEditor = () => {
   const [imageFiles, setImageFiles] = useState<{ image: File; id: string }[]>(
     []
+  );
+  const [selectedBlog, setSelectedBlog] = useState<string>(
+    dummyCategoriesData[0].blog
+  );
+  const [selectedCategory, setSelectedCategory] = useState<string>(
+    dummyCategoriesData[0].categories[0]
   );
 
   console.log(imageFiles);
@@ -142,38 +205,86 @@ const BlogEditor = () => {
   });
 
   return (
-    <div className="flex flex-col w-full p-4">
-      <input
-        type="text"
-        placeholder={"제목을 입력하세요"}
-        className={"focus:outline-none"}
-      />
-      <EditorContent
-        editor={editor}
-        className={"flex-1 overflow-y-auto text-primary-strong "}
-      />
-      <div className={"flex gap-4"}>
-        <button
-          onClick={() => {
-            if (editor) {
-              onSave(editor.getJSON());
+    <>
+      <div className="flex flex-col w-full p-4">
+        <div className="flex flex-col gap-5 w-full justify-center py-5">
+          <div className={"flex flex-col gap-2"}>
+            <input
+              type="text"
+              placeholder={"제목을 입력하세요"}
+              className={
+                "focus:outline-none font-bold text-[36px] placeholder:text-primary-weak text-primary-strong"
+              }
+            />
+            <div className={"w-[50px] h-2 shrink-0 bg-primary-strong"}></div>
+          </div>
+          <div className={"flex gap-4"}>
+            <div className={"px-4 py-1.5 rounded bg-primary-strong"}>
+              <select
+                className={
+                  "text-primary-white focus:outline-none font-bold bg-primary-strong"
+                }
+                onChange={(e) => {
+                  setSelectedBlog(e.target.value);
+                }}
+              >
+                {dummyCategoriesData.map(({ blog }) => (
+                  <option value={blog} key={blog}>
+                    {blog}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className={"px-4 py-1.5 rounded bg-primary-strong"}>
+              <select
+                className={
+                  "text-primary-white focus:outline-none font-bold bg-primary-strong"
+                }
+                onChange={(e) => {
+                  setSelectedCategory(e.target.value);
+                }}
+              >
+                {dummyCategoriesData
+                  .find(({ blog }) => selectedBlog === blog)
+                  ?.categories.map((category) => (
+                    <option key={category} value={category}>
+                      {category}
+                    </option>
+                  ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <hr className={"py-2"} />
+
+        <EditorContent
+          editor={editor}
+          className={"flex-1 overflow-y-auto text-primary-strong "}
+        />
+        <div className={"flex gap-4"}>
+          <button
+            onClick={() => {
+              if (editor) {
+                onSave(editor.getJSON());
+              }
+            }}
+            className={
+              "px-4 py-1.5 rounded bg-primary-strong text-white brightness-100 hover:bg-primary-normal"
             }
-          }}
-          className={
-            "px-4 py-1.5 rounded bg-primary-strong text-white brightness-100 hover:bg-primary-normal"
-          }
-        >
-          저장
-        </button>
-        <button
-          className={
-            "px-4 py-1.5 rounded bg-primary-strong text-white brightness-100 hover:bg-primary-normal"
-          }
-        >
-          취소
-        </button>
+          >
+            저장
+          </button>
+          <button
+            className={
+              "px-4 py-1.5 rounded bg-primary-strong text-white brightness-100 hover:bg-primary-normal"
+            }
+          >
+            취소
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
